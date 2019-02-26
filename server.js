6f8f11043d85c128868c037bb0fa9e844cfb2619
Timothy Cavinder <Timothycavinder@msn.com>
2019-02-25 21:09:43 -0500

    Fix again

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const port = process.env.PORT || 8082;
+const app = express();
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + '/dist/'));
+
 app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + "/dist/index.html" );
+    res.sendFile(__dirname + '/dist/index.html' );
 });
+
 app.listen(port);
-console.log('Server started . . .');
