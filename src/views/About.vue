de0ec863d81b7386df693713dd0766ba153e7225
Timothy Cavinder <Timothycavinder@msn.com>
2019-02-27 13:24:22 -0500

    added news

--- src/views/About.vue
+++ src/views/About.vue
@@ -33,7 +33,7 @@
         >
           Weather Data
         </button>
-        <!--    <input type="submit" id="btn-lookup" class="btn btn-info" value="My Weather">-->
+       
       </div>
     </form>
             </div>
@@ -42,7 +42,36 @@
  
         </div>
 
-  
+  <div class="container center">
+    <div class="row">
+       <button
+          title="get news"
+          type="button"
+          class="btn btn-info btn-block"
+          value="News"
+          v-on:click="getNews"
+          return: false
+        >
+          Get News
+        </button>
+      <div class="col-md-6 mid">
+        <div class="card card-body">
+          <div class="news"></div>
+           <div class="newstitle"></div>
+             <div class="description"></div>
+              <a href="" id="link">Full Story Here</a>
+               <div> <img src="" id="news_img"></div>
+        </div>
+         <div class="card card-body">
+          <div class="news2"></div>
+           <div class="newstitle2"></div>
+             <div class="description2"></div>
+              <a href="" id="link2">Full Story Here</a>
+               <div> <img src="" id="news_img2"></div>
+        </div>
+      </div>
+    </div>
+  </div>
  
   </div>
 </template>
@@ -98,7 +127,79 @@
      
     },
    
-  }
+  
+           getNews() {
+            let nws;
+            const url = 'https://newsapi.org/v2/top-headlines?' +
+                 'country=us'+
+                '&apiKey=3f12352d060244a3909c4ac92ce96aae';
+            const req = new Request(url);
+            fetch(req)
+                .then((response) => response.json())
+            .then(data => {
+                nws = data;
+            $('.news').text(data.articles[0].source.name);
+            $('.newstitle').text(data.articles[0].title);
+            $('.description').text(data.articles[0].description);
+            /*  $('#news_img').src(data.articles[0].urlToImage);*/
+            document.getElementById("news_img").src=data.articles[0].urlToImage;
+
+            let link= data.articles[0].url;
+            document.getElementById("link").href = link;
+            /*     $('.link').text(data.articles[0].url); */
+
+            $('.news2').text(data.articles[1].source.name);
+            $('.newstitle2').text(data.articles[1].title);
+            $('.description2').text(data.articles[1].description);
+            /*  $('#news_img').src(data.articles[0].urlToImage);*/
+            document.getElementById("news_img2").src=data.articles[1].urlToImage;
+
+            let link2= data.articles[1].url;
+            document.getElementById("link2").href = link2;
+
+            /*    $('.news3').text(data.articles[2].source.name);
+                $('.newstitle3').text(data.articles[2].title);
+                $('.description3').text(data.articles[2].description);
+                /*  $('#news_img').src(data.articles[0].urlToImage);*/
+             /*   document.getElementById("news_img3").src=data.articles[2].urlToImage;
+
+                let link3= data.articles[2].url;
+                document.getElementById("link3").href = link3;
+
+                $('.news4').text(data.articles[3].source.name);
+                $('.newstitle4').text(data.articles[3].title);
+                $('.description4').text(data.articles[3].description);
+                /*  $('#news_img').src(data.articles[0].urlToImage);*/
+            /*    document.getElementById("news_img4").src=data.articles[3].urlToImage;
+
+                let link4= data.articles[3].url;
+                document.getElementById("link4").href = link4;
+/*-----------------------------------------------------------------------------*/
+           /*     $('.news5').text(data.articles[4].source.name);
+                $('.newstitle5').text(data.articles[4].title);
+                $('.description5').text(data.articles[4].description);
+                /*  $('#news_img').src(data.articles[0].urlToImage);*/
+            /*    document.getElementById("news_img5").src=data.articles[4].urlToImage;
+
+                let link5= data.articles[4].url;
+                document.getElementById("link5").href = link5;
+
+                /*-----------------------------------------------------------------------------*/
+
+            /*    $('.news6').text(data.articles[5].source.name);
+                $('.newstitle6').text(data.articles[5].title);
+                $('.description6').text(data.articles[5].description);
+                /*  $('#news_img').src(data.articles[0].urlToImage);*/
+             //   document.getElementById("news_img6").src=data.articles[5].urlToImage;
+
+         //       let link6= data.articles[5].url;
+         //       document.getElementById("link6").href = link6;
+
+         //   console.log(data);
+            });
+
+           }
+   },  
  }
  </script>
  
