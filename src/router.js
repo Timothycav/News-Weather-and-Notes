fa170a5e8eda3e63c92db062942a45f865cf85c8
Timothy Cavinder <Timothycavinder@msn.com>
2019-03-08 11:29:48 -0500

    sign

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import firebase from 'firebase'
 import Display from './views/Display.vue'
 
 Vue.use(Router)
